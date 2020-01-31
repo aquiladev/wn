b4945bbb9b0ab278d4f78c3e3c4c602ea6a96717
aquiladev <aquiladev@users.noreply.github.com>
2020-01-31 12:29:35 +0000

    Automated deployment: Fri Jan 31 12:29:35 UTC 2020 870f95629f5a97bd8b34ac36e1948c4d6ff6b92a

---
+++ precache-manifest.fc0b0095337c88a93d21d0fc5ac57a01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc61ec68c5e0929679867bdaba553532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04f23de7969cbe1120f7",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fa52cb8183a3ad5484eb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04f23de7969cbe1120f7",
+    "url": "/static/js/2.af4b236e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.af4b236e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa52cb8183a3ad5484eb",
+    "url": "/static/js/main.e1497980.chunk.js"
+  },
+  {
+    "revision": "77f99a2826fff8f0d0a5",
+    "url": "/static/js/runtime-main.cc7dc051.js"
+  },
+  {
+    "revision": "d5a8f8f462d9b261fd2b5f030a5aff07",
+    "url": "/static/media/for_dev.d5a8f8f4.jpg"
+  },
+  {
+    "revision": "a6ab1e747b13f82721ef49dee5822d81",
+    "url": "/static/media/for_startup.a6ab1e74.jpg"
+  }
+]);
